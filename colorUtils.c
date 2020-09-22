5bb1200bb6f5cec41c4ede690df3472aee0ee6dc
Lolson31 <69780105+Lolson31@users.noreply.github.com>
2020-09-22 17:57:18 -0500

    added luminosity, min, and sepiaRed funcs

--- colorUtils.c
+++ colorUtils.c
@@ -1,12 +1,31 @@
 
 
-int max(int x, int y, int z) {
+int max(int x, int y, int z)
+{
   int m = x > y ? x : y;
   m = m > z ? m : z;
   return m;
 }
 
-int toGrayScaleAverage(int r, int g, int b) {
+int min(int a, int b, int c)
+{
+  int min = a < b ? a : b;
+  min = m < c ? min : c;
+  return min;
+}
+
+int toGrayScaleLuminosity(int r, int g, int b)
+{
+  return (0.21 * r) + (0.72 * g) + (0.07 * b);
+}
+
+int toGrayScaleAverage(int r, int g, int b)
+{
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
 }
+
+ int toSepiaRed(int r, int g, int b)
+ {
+   return (0.393 * r) + (0.769 * g) + (0.189 * b);
+ }
--- colorUtils.h
+++ colorUtils.h
@@ -3,11 +3,29 @@
  * Returns the maximum value among the three
  * given integer values.
  */
-int max(int x, int y, int z);
+ int max(int x, int y, int z);
+
+/**
+ * Returns the minimum value among the three
+ * given interger values
+ */
+ int min(int a, int b, int c);
 
 /**
  * Returns a gray-scaled RGB value of the three
  * given RGB values using the average technique.
  *
  */
-int toGrayScaleAverage(int r, int g, int b);
+ int toGrayScaleAverage(int r, int g, int b);
+
+/**
+ * Returns a gray-scaled RGB value of the three
+ * given RGB values using luminosity technique
+ */
+ int toGrayScaleLuminosity(int r, int g, int b);
+
+ /**
+  * Returns the red sepia-toned RGB value of the
+  * three given RGB values
+  */
+  int toSepiaRed(int r, int g, int b)
