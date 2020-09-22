2938b94a78167b9490cc55069eb24a6bd2b20809
Lolson31 <69780105+Lolson31@users.noreply.github.com>
2020-09-22 18:02:32 -0500

    cleaned code, added sepiaGreen and Blue funcs

--- colorUtils.c
+++ colorUtils.c
@@ -1,31 +1,41 @@
 
 
-int max(int x, int y, int z)
+int max (int x, int y, int z)
 {
   int m = x > y ? x : y;
   m = m > z ? m : z;
   return m;
 }
 
-int min(int a, int b, int c)
+int min (int a, int b, int c)
 {
   int min = a < b ? a : b;
   min = m < c ? min : c;
   return min;
 }
 
-int toGrayScaleLuminosity(int r, int g, int b)
+int toGrayScaleLuminosity (int r, int g, int b)
 {
   return (0.21 * r) + (0.72 * g) + (0.07 * b);
 }
 
-int toGrayScaleAverage(int r, int g, int b)
+int toGrayScaleAverage (int r, int g, int b)
 {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
 }
 
- int toSepiaRed(int r, int g, int b)
+ int toSepiaRed (int r, int g, int b)
  {
    return (0.393 * r) + (0.769 * g) + (0.189 * b);
+ }
+
+ int toSepiaGreen (int r, int g, int b)
+ {
+   return (0.349 * r) + (0.686 * g) + (0.168 * b);
+ }
+
+ int toSepiaBlue (int r, int g, int b)
+ {
+   return (0.272 * r) + (0.534 * g) + (0.131 * b);
  }
--- colorUtils.h
+++ colorUtils.h
@@ -3,29 +3,41 @@
  * Returns the maximum value among the three
  * given integer values.
  */
- int max(int x, int y, int z);
+ int max (int x, int y, int z);
 
 /**
  * Returns the minimum value among the three
  * given interger values
  */
- int min(int a, int b, int c);
+ int min (int a, int b, int c);
 
 /**
  * Returns a gray-scaled RGB value of the three
  * given RGB values using the average technique.
  *
  */
- int toGrayScaleAverage(int r, int g, int b);
+ int toGrayScaleAverage (int r, int g, int b);
 
 /**
  * Returns a gray-scaled RGB value of the three
  * given RGB values using luminosity technique
  */
- int toGrayScaleLuminosity(int r, int g, int b);
+ int toGrayScaleLuminosity (int r, int g, int b);
 
  /**
   * Returns the red sepia-toned RGB value of the
   * three given RGB values
   */
-  int toSepiaRed(int r, int g, int b)
+  int toSepiaRed (int r, int g, int b)
+
+  /**
+   * Returns the green sepia-toned RGB value of the
+   * three given RGB values
+   */
+   int toSepiaGreen (int r, int g, int b)
+
+   /**
+    * Returns the blue sepia-toned RGB value of the
+    * three given RGB values
+    */
+    int toSepiaBlue (int r, int g, int b)
